73d5576daacfbb6102e73ff122c8cd868da91110
MgAsma <asma@ekfrazo.in>
2025-03-28 21:55:27 +0530

    accessbility minor issue fixed

--- src/app/activate-child.guard.ts
+++ src/app/activate-child.guard.ts
@@ -3,7 +3,6 @@ import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot }
 import { SubModuleService } from './service/sub-module.service';
 import isOnline from 'is-online';
 import { lastValueFrom } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,43 +19,34 @@ export class ActivateChildGuard implements CanActivateChild {
       this._router.navigate(['/no-internet']);
       return false;
     }
-
+  
     const userId = sessionStorage.getItem('user_id');
-    const roleName = sessionStorage.getItem('user_role_name')?.toLowerCase(); 
-
+    const roleName = sessionStorage.getItem('user_role_name')?.toLowerCase();
+  
     if (!userId) {
       this._router.navigate(['/login']);
       return false;
     }
+
     if (roleName === 'admin') {
       return true;
     }
-
+  
     try {
-      const access = await lastValueFrom(
-        this.subModuleService.getAccessForActiveUrl(+userId, state.url).pipe(
-          map((access: any[]) => {
-            if (access && access.length > 0) {
-              const hasViewPermission = Boolean(
-                access[0].operations?.some((op: any) => op.view === true)
-              );
-
-              if (hasViewPermission) {
-                return true;
-              }
-            }
-
-            this._router.navigate(['/unauthorized']);
-            return false;
-          })
-        )
-      );
-
-      return access;
+      const access = await lastValueFrom(this.subModuleService.getAccessForActiveUrl(+userId, state.url));
+      let hasViewPermission = false
+      if (access && access.length > 0) {
+          hasViewPermission = access.some((item: any) =>
+          item.operations?.some((op: any) => op.view === true)
+        );
+      }
+  
+      
+      return hasViewPermission;
     } catch (error) {
-      console.error('Error fetching access permissions:', error);
       this._router.navigate(['/unauthorized']);
       return false;
     }
   }
+  
 }
