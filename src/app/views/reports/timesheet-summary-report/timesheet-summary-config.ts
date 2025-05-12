93b73e52c775f915480532bdb3bd5ce663b9a556
Asma <asma@dataseedtech.com>
2025-05-12 17:27:18 +0530

    summary report day column hyper link enabled

--- src/app/views/reports/timesheet-summary-report/timesheet-summary-config.ts
+++ src/app/views/reports/timesheet-summary-report/timesheet-summary-config.ts
@@ -2,14 +2,14 @@
 
   export const tableColumns = [
     { label: 'Sl No', key: 'sl' },
-    { label: 'Employee', key: 'employee_name', keyId:'employee_id', sortable: true ,filterable: true, navigation:true, filterType: 'multi-select' },
-    { label: 'Sun', key: 'Sunday', sortable: true },
-    { label: 'Mon', key: 'Monday', sortable: true },
-    { label: 'Tues', key: 'Tuesday', sortable: true },
-    { label: 'Wed', key: 'Wednesday', sortable: true },
-    { label: 'Thu', key: 'Thursday', sortable: true },
-    { label: 'Fri', key: 'Friday', sortable: true },
-    { label: 'Sat', key: 'Saturday', sortable: true },
+    { label: 'Employee', key: 'employee_name', keyId:'employee_id', sortable: true ,filterable: true, filterType: 'multi-select' },
+    { label: 'Sun', key: 'Sunday', sortable: true,navigation:true},
+    { label: 'Mon', key: 'Monday', sortable: true,navigation:true },
+    { label: 'Tues', key: 'Tuesday', sortable: true,navigation:true },
+    { label: 'Wed', key: 'Wednesday', sortable: true,navigation:true },
+    { label: 'Thu', key: 'Thursday', sortable: true,navigation:true },
+    { label: 'Fri', key: 'Friday', sortable: true,navigation:true },
+    { label: 'Sat', key: 'Saturday', sortable: true,navigation:true },
     { label: 'Total Time', key: 'employee_worked_hours', sortable: true },
     { label: 'Shortfall', key: 'short_fall', sortable: true }
   ];
