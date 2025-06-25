034bad12c08018337360442659466e00042b5e7d
Arpitha-Gowri <143486020+22arpitha@users.noreply.github.com>
2025-06-25 11:16:22 +0530

    Added start and end date to the work calendar and validation is done

--- src/app/views/leave/work-calendar/work-calendar.component.ts
+++ src/app/views/leave/work-calendar/work-calendar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonServiceService } from '../../../service/common-service.service';
 import { ApiserviceService } from '../../../service/apiservice.service';
 import { SubModuleService } from '../../../service/sub-module.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from '../../../../environments/environment';
 @Component({
   selector: 'app-work-calendar',
@@ -12,6 +12,7 @@ import { environment } from '../../../../environments/environment';
 export class WorkCalendarComponent implements OnInit {
   BreadCrumbsTitle: any = 'Work Calendar';
   workCalendarForm: FormGroup
+  minDate: any;
   weeks = [
     { week: 'Sunday' },
     { week: 'Monday' },
@@ -19,7 +20,7 @@ export class WorkCalendarComponent implements OnInit {
     { week: 'Wednesday' },
     { week: 'Thursday' },
     { week: 'Friday' },
-    { week: 'Sunday' }
+    { week: 'Saturday' }
   ]
   definedWeekend = [
     { week: 'Sunday', all: true, first: true, second: true, third: true, fourth: true, fifth: true },
@@ -28,7 +29,7 @@ export class WorkCalendarComponent implements OnInit {
     { week: 'Wednesday', all: false, first: false, second: false, third: false, fourth: false, fifth: false },
     { week: 'Thursday', all: false, first: false, second: false, third: false, fourth: false, fifth: false },
     { week: 'Friday', all: false, first: false, second: false, third: false, fourth: false, fifth: false },
-    { week: 'Sunday', all: false, first: false, second: false, third: false, fourth: false, fifth: false }
+    { week: 'Saturday', all: false, first: false, second: false, third: false, fourth: false, fifth: false }
   ]
   constructor(
     private common_service: CommonServiceService,
@@ -46,16 +47,39 @@ export class WorkCalendarComponent implements OnInit {
 
   initialform() {
     this.workCalendarForm = this.fb.group({
-      week_starts_on: [''],
-      work_week_starts_on: [''],
-      work_week_ends_on: [''],
-      half_work_day: ['']
+      week_starts_on: ['', Validators.required],
+      work_week_starts_on: ['', Validators.required],
+      work_week_ends_on: ['', Validators.required],
+      current_year: ['', Validators.required],
+      custom_start_date: [''],
+      custom_end_date: [''],
     })
   }
 
   get f() {
     return this.workCalendarForm.controls;
   }
+  onRadioChange(event: any): void {
+    console.log('Selected value:', event.value);
+    const startCtrl = this.workCalendarForm?.get('custom_start_date');
+    const endCtrl = this.workCalendarForm?.get('custom_end_date');
+    if (event.value != 'Custom') {
+      startCtrl?.clearValidators();
+      endCtrl?.clearValidators();
+      startCtrl?.setValue(null);
+      endCtrl?.setValue(null);
+      this.minDate = '';
+    } else {
+      startCtrl?.setValidators([Validators.required]);
+      endCtrl?.setValidators([Validators.required]);
+    }
+    startCtrl?.updateValueAndValidity();
+    endCtrl?.updateValueAndValidity();
+  }
+  startDateFun(event) {
+    this.minDate = event.value
+  }
+  
   onAllSelect(day: any) {
     const val = day.all;
     day.first = val;
@@ -64,19 +88,29 @@ export class WorkCalendarComponent implements OnInit {
     day.fourth = val;
     day.fifth = val;
   }
-  OnSingleSelection(day:any){
+  OnSingleSelection(day: any) {
     // if()
     console.log(day)
-    if(day.first && day.second && day.third && day.fourth && day.fifth){
+    if (day.first && day.second && day.third && day.fourth && day.fifth) {
       day.all = true;
-    } else{
+    } else {
       day.all = false;
     }
-      day.first = day.first;
-      day.second = day.second;
-      day.third = day.third;
-      day.fourth = day.fourth;
-      day.fifth = day.fifth;
-      // console.log(this.definedWeekend)
+    day.first = day.first;
+    day.second = day.second;
+    day.third = day.third;
+    day.fourth = day.fourth;
+    day.fifth = day.fifth;
+    // console.log(this.definedWeekend)
+  }
+  saveWorkCalendarData() {
+    if (this.workCalendarForm.invalid) {
+      this.workCalendarForm.markAllAsTouched();
+    } else {
+      console.log(this.workCalendarForm.value)
+    }
+  }
+  cancelAndBack() {
+
   }
 }
